dec2f59d130aea61ea513c21badc0d9211587cca
d11416146 <d11416146@gs.takming.edu.tw>
2026-04-14 14:09:04 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Person
+{
+    public virtual string GetName()
+    {
+        return "Person";
+    }
+}
+public class Student : Person
+
+{
+    public override string GetName()
+    {
+        return "Student";
+    }
+}
+public class Teacher : Person
+{
+    public new string GetName()
+    {
+        return "Teacher";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace class_0414_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s=new Student();
+            Console.WriteLine(s.GetName());
+
+            Person p=new Student();
+            Console.WriteLine(p.GetName());
+
+            Teacher t=new Teacher();
+            Console.WriteLine(t.GetName());
+
+            Person p2 = new Teacher();
+            Console.WriteLine(p2.GetName());
+
+            Console.WriteLine(((Teacher)p2). GetName());
+
+
+        }
+    }
+}
